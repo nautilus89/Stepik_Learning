75606d6a79654ee53ee8855ad86a46e07dc92e91
nautilus89 <i08071989@gmail.com>
2020-12-28 18:07:13 +0300

    модуль с автотестами, запуск из cmd

--- Stepik_AutoTest/test_with_unittest.py
+++ Stepik_AutoTest/test_with_unittest.py
@@ -22,26 +22,16 @@ def fill_form_registration(link):
     try:
         browser = webdriver.Chrome()
         browser.get(link)
-
-        input1 = browser.find_element_by_css_selector(".first_block .first")
-        input1.send_keys("Ivan")
-        input2 = browser.find_element_by_css_selector(".first_block .second")
-        input2.send_keys("Petrov")
-        input3 = browser.find_element_by_css_selector(".first_block .third")
-        input3.send_keys("ipetrov@test.ru")
-
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
+        browser.find_element_by_css_selector(".first_block .first").send_keys("Ivan")
+        browser.find_element_by_css_selector(".first_block .second").send_keys("Petrov")
+        browser.find_element_by_css_selector(".first_block .third").send_keys("ipetrov@test.ru")
+        browser.find_element_by_css_selector("button.btn").click()
 
         time.sleep(2)
 
         # находим элемент, содержащий текст
-        welcome_text_elt = browser.find_element_by_tag_name("h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
-        return welcome_text_elt.text
-
+        return browser.find_element_by_tag_name("h1").text
     finally:
-        time.sleep(5)
         browser.quit()
 
 
